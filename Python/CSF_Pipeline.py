690231ced6b4183d788d4593b20268fa106ea646
Nicholas Riley Zidel <rileyzidel@gmail.com>
2025-04-07 21:38:43 -0400

    Created the CSFData.py file. the getter class handles the reading and preprocessing of the CSF data. Also removed any related functions from the CSF_Pipeline.py file to avoid confusion.

--- Python/CSF_Pipeline.py
+++ Python/CSF_Pipeline.py
@@ -3,11 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xgboost as xgb
 import graphviz
+import CSFData
 from matplotlib.patches import Patch
 from sklearn import clone, svm
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_selection import mutual_info_classif, VarianceThreshold, SelectKBest
-from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split, StratifiedKFold, cross_validate
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.metrics import confusion_matrix, roc_curve, auc, RocCurveDisplay, roc_auc_score
@@ -18,45 +16,6 @@ class Model:
     def __init__(self, X, y):
         self.X = X
         self.y = y
-
-    def cleanData(self, nathresh=.5, k=5):
-        """
-        cleanData modifies Model.X to remove columns containing more than nathresh % missing values. 
-        Any missing values that are not removed are imputed using K Nearest Neigbors.
-
-        Parameters:
-            nathresh: Double Default = .5 (50%); threshold of missing values permitted before a column is dropped
-            k: int Default = 5; number of neigbors to be used for KNNImputer
-        """
-
-        nathresh = .5        # % of samples allowed to be NA before column is dropped
-        self.X = self.X.dropna(axis=1, thresh=int((1 - nathresh) * self.X.shape[0]))
-
-        #NOTE: Optimize k value
-
-        imputer = KNNImputer(weights='distance', n_neighbors=k)
-        imputer.set_output(transform='pandas')
-        self.X = imputer.fit_transform(self.X, self.y)
-
-    def featureSelector(self, threshold = 0.0, k = 15):
-        """featureSelector returns a subset of k features(columns) from X, based on the
-        value of threshold (used for sklearn.feature_selection VarianceThreshold) and
-        mutual info classification.
-
-        Parameters:
-            threshold: float; default 0. featues with variance less than this value will be ignored
-            k: int; number of features to be selected
-        
-        """
-        sel = VarianceThreshold(threshold=threshold)
-        sel.set_output(transform="pandas")
-        self.X = sel.fit_transform(self.X, self.y) #Removes low variance features
-
-        sel = SelectKBest(mutual_info_classif, k=k)
-        mic_params = sel.get_params()
-        mic_params["random_state"] = 42
-        sel.set_output(transform="pandas")
-        self.X = sel.fit_transform(self.X, self.y) #Removes features based on mutual info classifer
     
     def rf(self, n=1000, n_splits=5):
         """rf (Random Forest) prints the training and testing scores of a 'n' tree random 
@@ -231,72 +190,21 @@ class dataVisualization:
         plt.grid(True)
         plt.show()
 
-
-
-
-
-def getdata(path="CSF-Research-Project\Data\FORD-0101-21ML+ DATA TABLES_CSF (METADATA UPDATE).XLSX", datasheet=3):
-    """ 
-    getdata reads from the CSF excel data sheet and returns a Dataframe with the Class data attribute
-    attached.
-
-    Parameters:
-        path: Default is the Data folder with the data in it
-        datasheet: int 1-3; refers to the sheet within the excel document that the data is to be pulled from
-            1: Batch-normalized Data
-            2: Batch-norm Imputed Data
-            3: Log Transformed Data
-    Returns:
-        DataFrame containing with column 0 as the class data and all following columns as attributes
-    """
-    mapping = {1:'Batch-normalized Data', 2: 'Batch-norm Imputed Data', 3:'Log Transformed Data'}
-    patient_data = pd.read_excel(
-        path,
-        sheet_name = "Sample Meta Data",
-        header=0,
-        usecols = ["PARENT_SAMPLE_NAME", "COHORT", "PPMI_CLINICAL_EVENT", "PPMI_COHORT"],
-        index_col="PARENT_SAMPLE_NAME"
-    )
-    patient_data = patient_data.drop(patient_data[
-        (patient_data.COHORT != "PPMI") |
-        (patient_data.PPMI_CLINICAL_EVENT != "BL")
-    ].index)
-    patient_data = patient_data.drop("COHORT", axis=1)
-    patient_data = patient_data.drop("PPMI_CLINICAL_EVENT", axis=1)
-
-    df = pd.read_excel(
-        path,
-        sheet_name = mapping.get(datasheet),
-        index_col="PARENT_SAMPLE_NAME"
-    )
-
-    df = patient_data.join(df, on="PARENT_SAMPLE_NAME", how='inner')
-    return df
-
-
-data = getdata(datasheet=1)
-data.columns = data.columns.astype(str)
-
-y = data["PPMI_COHORT"]
-X = data.drop(data.columns[0:1], axis=1)
-
-le = LabelEncoder()
-y = le.fit_transform(y)
+g = CSFData.getter()
+X, y = g.getXy()
 
 m = Model(X, y)
-m.cleanData() #NOTE report on the differences in imputation methods
-m.featureSelector(k=15)
-# m.rf(n_splits=10)
-m.xgboost(n_splits=10)
+m.rf(n_splits=10)
+#m.xgboost(n_splits=10)
 
-# for i in range(5, 30, 5):
-#    for j in range(250, 2000, 250):
-#        print("Number of metabolites: " + str(i))
-#        print("Number of RF Trees:    " + str(j))
-#        m = Model(X, y)
-#        m.cleanData()
-#        m.featureSelector(k=i)
-#        m.rf(n=j, n_splits=10)
+#for i in range(5, 30, 5):
+#   for j in range(250, 2000, 250):
+#       print("Number of metabolites: " + str(i))
+#       print("Number of RF Trees:    " + str(j))
+#       m = Model(X, y)
+#       m.cleanData()
+#       m.featureSelector(k=i)
+#       m.rf(n=j, n_splits=10)
 
 
 
