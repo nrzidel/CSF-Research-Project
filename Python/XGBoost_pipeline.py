debd980a14ad735f6f2272c00702b436bb4ecc77
Kyle Ventura <kyventur@umich.edu>
2025-04-12 19:13:19 -0400

    pipeline

--- Python/XGBoost_pipeline.py
+++ Python/XGBoost_pipeline.py
@@ -18,83 +18,7 @@ from skopt.space import Real, Categorical, Integer
 from xgboost import plot_importance
 from CSFData import getter
 
-loop_params = False
-
-
-# data = pd.read_csv("Data\PPMI_Cohort_Filtered.csv")
-# data = data.drop(data.columns[0], axis=1)   #Drop the first column, which is the sequential numbers from R
-
-# y = data["PPMI_COHORT"].values
-# X = data.iloc[:, 3:]
-
-# le = LabelEncoder()
-# y = le.fit_transform(y)
-
-# data = Process_Data(X,y)
-# data.cleanData()
-# data.featureSelector(0.9,50)
-# data = getter()
-# X, y = data.getXy()
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
-
-# estimators = [
-#     # ('encoder', TargetEncoder()),
-#     ('clf', XGBClassifier(random_state=8)) # can customize objective function with the objective parameter
-# ]
-# pipe = Pipeline(steps=estimators)
-
-# search_space = {
-#     'clf__booster': ("gbtree", "dart", "gblinear"),
-#     'clf__min_child_weight': Real(0.0, 10),
-#     'clf__max_delta_step': Integer(0, 10),
-#     'clf__subsample': Real(0, 1),
-#     'clf__max_depth': Integer(2,8),
-#     'clf__learning_rate': Real(0.001, 1.0, prior='log-uniform'),
-#     'clf__subsample': Real(0.5, 1.0),
-#     'clf__colsample_bytree': Real(0.5, 1.0),
-#     'clf__colsample_bylevel': Real(0.5, 1.0),
-#     'clf__colsample_bynode' : Real(0.5, 1.0),
-#     'clf__reg_alpha': Real(0.0, 10.0),
-#     'clf__reg_lambda': Real(0.0, 10.0),
-#     'clf__gamma': Real(0.0, 10.0),
-#     # 'clf__tree_method': ("exact", "approx", "hist"),
-#     'clf__max_leaves': Integer(0, 10),
-#     'clf__num_parallel_tree': Integer(1,5)
-
-# }
-
-# opt = BayesSearchCV(pipe, search_space, cv=5, n_iter=15, scoring='roc_auc', random_state=42) 
-# # in reality, you may consider setting cv and n_iter to higher values
-# opt.fit(X_train, y_train)
-# print(opt.best_estimator_)
-# print(opt.best_score_)
-# print(opt.score(X_test, y_test))
-# opt.predict(X_test)
-# opt.predict_proba(X_test)
-# opt.best_estimator_.steps
-# xgboost_step = opt.best_estimator_.steps[0]
-# xgboost_model = xgboost_step[1]
-# plot_importance(xgboost_model)
-
-# (base_score=None, booster=None, callbacks=None,   
-#                                colsample_bylevel=0.9777389931549642,
-#                                colsample_bynode=0.8503107223106829,
-#                                colsample_bytree=0.9358259642316076, device=None,
-#                                early_stopping_rounds=None,
-#                                enable_categorical=False, eval_metric=None,
-#                                feature_types=None, feature_weights=None,
-#                                gamma=4.158210875794536, grow_policy=None,
-#                                importance_type=None,
-#                                interaction_constraints=None,
-#                                learning_rate=0.01390574606467376, max_bin=None,
-#                                max_cat_threshold=None, max_cat_to_onehot=None,
-#                                max_delta_step=None, max_depth=3,
-#                                max_leaves=None, min_child_weight=None,
-#                                missing=nan, monotone_constraints=None,
-#                                multi_strategy=None, n_estimators=None,
-#                                n_jobs=None, num_parallel_tree=None, ...))])
-
+loop_params = True
 
 estimators = [
     # ('encoder', TargetEncoder()),
@@ -122,7 +46,7 @@ search_space = {
 
 }
 
-opt = BayesSearchCV(pipe, search_space, cv=5, n_iter=15, scoring='roc_auc', random_state=42) 
+opt = BayesSearchCV(pipe, search_space, cv=5, n_iter=25, scoring='roc_auc', random_state=42) 
 
 feature_dictionary = {}
 
@@ -163,12 +87,13 @@ if loop_params:
                             best_inputs = [sheet, thresh, knn, varthresh, kselect]
                         importances = opt.best_estimator_.steps[0][1].feature_importances_
                         named_importances = list(zip(data.get_X_columns(), importances))
-
-                        for i in range(10):
-                            if named_importances[i][0] in feature_dictionary.keys():
-                                feature_dictionary[named_importances[i][0]]+=1
-                            else:
-                                feature_dictionary[named_importances[i][0]]=1
+                        
+                        if opt.best_score_ >= 0.7:
+                            for i in range(10):
+                                if named_importances[i][0] in feature_dictionary.keys():
+                                    feature_dictionary[named_importances[i][0]]+=1
+                                else:
+                                    feature_dictionary[named_importances[i][0]]=1
                         
                         current_run+=1
                         print("Run {} of {}".format(current_run, num_runs))
@@ -195,8 +120,6 @@ else:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
     opt.fit(X_train, y_train)
     
-    # in reality, you may consider setting cv and n_iter to higher values
-
     xgboost_step = opt.best_estimator_.steps[0]
     xgboost_model = xgboost_step[1]
   
