17654ea0ec84c052b5ef573197bbf6c9ad35f2ec
Nicholas Zidel <nicholaszidel@gmail.com>
2025-04-14 18:35:40 -0400

    Modified CSFData class to correct data leakage.
    Removed: FeatureSelector()
    Removed: knn parameter from cleanData()

--- Python/CSFData.py
+++ Python/CSFData.py
@@ -11,7 +11,7 @@ class getter:
         self.y = data["PPMI_COHORT"]
         self.X = data.drop(data.columns[0:1], axis=1)
     
-    def getdata(self, path="C:/Users/kvent/Documents/CSF-Research-Project\Data\FORD-0101-21ML+ DATA TABLES_CSF (METADATA UPDATE).XLSX", datasheet=3, group = "BL"):
+    def getdata(self, path=".\Data\FORD-0101-21ML+ DATA TABLES_CSF (METADATA UPDATE).XLSX", datasheet=3, group = "BL"):
         """ 
         getdata reads from the CSF excel data sheet and returns a Dataframe with the Class data attribute
         attached.
@@ -50,54 +50,24 @@ class getter:
         df = patient_data.join(df, on="PARENT_SAMPLE_NAME", how='inner')
         return df
     
-    def cleanData(self, nathresh=.5, k=5):
+    def cleanData(self, nathresh=.5):
         """
         cleanData modifies Model.X to remove columns containing more than nathresh % missing values. 
         Any missing values that are not removed are imputed using K Nearest Neigbors.
 
         Parameters:
             nathresh: Double Default = .5 (50%); threshold of missing values permitted before a column is dropped
-            k: int Default = 5; number of neigbors to be used for KNNImputer
         """
-
-        nathresh = .5        # % of samples allowed to be NA before column is dropped
+        # % of samples allowed to be NA before column is dropped
         self.X = self.X.dropna(axis=1, thresh=int((1 - nathresh) * self.X.shape[0]))
 
-        #NOTE: Optimize k value
-
-        imputer = KNNImputer(weights='distance', n_neighbors=k)
-        imputer.set_output(transform='pandas')
-        self.X = imputer.fit_transform(self.X, self.y)
-
-    def featureSelector(self, threshold = 0.0, k = 15):
-        """featureSelector returns a subset of k features(columns) from X, based on the
-        value of threshold (used for sklearn.feature_selection VarianceThreshold) and
-        mutual info classification.
-
-        Parameters:
-            threshold: float; default 0. featues with variance less than this value will be ignored
-            k: int; number of features to be selected
-        
-        """
-        sel = VarianceThreshold(threshold=threshold)
-        sel.set_output(transform="pandas")
-        self.X = sel.fit_transform(self.X, self.y) #Removes low variance features
-
-        sel = SelectKBest(mutual_info_classif, k=k)
-        mic_params = sel.get_params()
-        mic_params["random_state"] = 42
-        sel.set_output(transform="pandas")
-        self.X = sel.fit_transform(self.X, self.y) #Removes features based on mutual info classifer
     
-    def getXy(self, nathresh=.5, knn=5, varthresh=0.0, kselect=15):
+    def getXy(self, nathresh=.5):
         """
-        getXy will call the cleanData and FeatureSelector methods and return the modified X and y dataframes
+        getXy will call cleanData and return X and y dataframes
 
         Parameters:
             nathresh: Double Default = .5 (50%); threshold of missing values permitted before a column is dropped
-            knn: int Default = 5; number of neigbors to be used for KNNImputer
-            varthresh: float; default = 0.0; featues with variance less than this value will be ignored
-            kselect: int; default = 15; number of features to be selected
 
         Returns:
             X: pandas dataframe containing kselect features.
@@ -107,8 +77,7 @@ class getter:
         le = LabelEncoder()
         self.y = le.fit_transform(self.y)
 
-        self.cleanData(nathresh=nathresh, k=knn)
-        self.featureSelector(threshold=varthresh, k = kselect)
+        self.cleanData(nathresh=nathresh)
 
         return self.X, self.y
 
