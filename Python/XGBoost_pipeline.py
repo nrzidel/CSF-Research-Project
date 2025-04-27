2023c7dccfa7d9f74541ffad716f93f84341d793
Kyle Ventura <kyventur@umich.edu>
2025-04-27 09:04:32 -0400

    Final XGBoost updates

--- Python/XGBoost_pipeline.py
+++ Python/XGBoost_pipeline.py
@@ -16,7 +16,7 @@ from xgboost import XGBClassifier
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
 from xgboost import plot_importance
-from CSFData_old import getter
+from CSFData import getter
 import pickle
 
 loop_params =  False
@@ -40,7 +40,7 @@ search_space = {
     'clf__subsample': Real(0, 1),
     'clf__max_depth': Integer(2,8),
     'clf__learning_rate': Real(0.001, 1.0, prior='log-uniform'),
-    'clf__subsample': Real(0.5, 1.0),
+    # 'clf__subsample': Real(0.5, 1.0),
     'clf__colsample_bytree': Real(0.5, 1.0),
     'clf__colsample_bylevel': Real(0.5, 1.0),
     'clf__colsample_bynode' : Real(0.5, 1.0),
@@ -57,27 +57,29 @@ best_models = []
 
 if loop_params:
 
-    pickle_name = "xgboost_sheet_1_n_iter_25_accuracy_v2"
-    num_runs = 180
+    pickle_name = "xgboost_sheet_1_n_iter_25_roc_auc_combined_score_cv10_v7"
+    num_runs = 144
     current_run = 0
     for sheet in [1]:
-        for thresh in [0.5]:
-            for knn in [5,10,15,20, 25, 30]:
-                for varthresh in [0.5, 0.6,0.7,0.8,0.9,1,]:
+        for thresh in [0.2, 0.35, 0.5]:
+            for knn in [5,10,15]:
+                for varthresh in [0.5,0.8,1]:
                     for kselect in [20,25,30,40,50]:
 
-                        opt = BayesSearchCV(pipe, search_space, cv=10, n_iter=25, scoring='accuracy', random_state=42) 
+                        opt = BayesSearchCV(pipe, search_space, cv=10, n_iter=25, scoring='roc_auc', random_state=42) 
                         data = getter(datasheet=sheet)
                         X, y = data.getXy(nathresh=thresh, knn = knn, varthresh=varthresh, kselect=kselect)
-                        X, y = data.getXy(nathresh=thresh)
                         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
                         opt.fit(X_train, y_train)
+                        data2 = getter(datasheet=1, group="V06")
+                        X2, y2 = data2.getXy_selectfeatures(columns=data.get_X_columns())
                         test_score = opt.score(X_test, y_test)
+                        v06_score = opt.score(X2, y2)
 
-                        best_models.append((test_score, opt, (sheet, thresh, knn, varthresh, kselect), data.get_X_columns()))
-                        best_models = sorted(best_models, key=lambda x: x[0], reverse=True)
-                        if len(best_models)>10:
-                            best_models = best_models[:10]
+                        best_models.append((opt.best_score_, opt, (sheet, thresh, knn, varthresh, kselect), data.get_X_columns(), test_score, v06_score))
+                        best_models = sorted(best_models, key=lambda x: (x[5],x[4],x[0]), reverse=True)
+                        if len(best_models)>20:
+                            best_models = best_models[:20]
                         
                         current_run+=1
                         print("Run {} of {}".format(current_run, num_runs))
@@ -93,7 +95,7 @@ if loop_params:
 
 else:
 
-    pickle_name = "xgboost_sheet_1_n_iter_25_roc_auc_v2"
+    pickle_name = "xgboost_sheet_1_n_iter_25_accuracy_best_score_cv10_v7"
     with open(pickle_name, 'rb') as file:
         best_models = pickle.load(file)
 
@@ -106,6 +108,7 @@ else:
         print("Best_Score: {}".format(model[0]))
         print(model_obj.best_estimator_)    
         print(named_importances)
+        print(model[2])
         # for i in range(len(named_importances)):
         #         if named_importances[i][0] in feature_dictionary.keys():
         #             feature_dictionary[named_importances[i][0]]+=named_importances[i][1]
@@ -116,13 +119,15 @@ else:
                     feature_dictionary[named_importances[i][0]]+=1
                 else:
                     feature_dictionary[named_importances[i][0]]=1
-    sorted_features = sorted(feature_dictionary, key=feature_dictionary.get, reverse=True)
+    best_features = {k:v for k, v in feature_dictionary.items() if v>=6}
+    sorted_features = sorted(best_features, key=best_features.get, reverse=True)
     print(sorted_features)
 
-    opt = BayesSearchCV(pipe, search_space, cv=5, n_iter=100, scoring='roc_auc', random_state=42) 
+    opt = BayesSearchCV(pipe, search_space, cv=10, n_iter=50, scoring='roc_auc', random_state=42) 
     data = getter(datasheet=1)
     # columns = ['100001178', '100004634', '100001992', '100006191', '999923644', '100001403', '100004208', '100001108', '100001605', '100021467', '100000894', '849', '100002927', '501', '999911299']
-    X, y = data.getXy_selectfeatures(columns=sorted_features[:20])
+    # X, y = data.getXy_selectfeatures(columns=sorted_features[:20])
+    X, y = data.getXy_selectfeatures(columns=sorted_features)
     # X, y = data.getXy_selectfeatures(columns=columns)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
     opt.fit(X_train, y_train)
@@ -144,8 +149,8 @@ else:
     for feature, importance in sorted_feature_importances:
         print(f"{feature}: {importance}")
     
-    with open("best_features_model_frequency_v2", 'wb') as file:
-        pickle.dump((opt.best_score_, opt, None, data.get_X_columns()), file)
+    with open("best_features_model_frequency_test", 'wb') as file:
+        pickle.dump([(opt.best_score_, opt, None, data.get_X_columns())], file)
     
     opt = best_models[0][1]
     data = getter(datasheet=1)
